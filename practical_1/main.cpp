3370f54b143617a583c31671a389063b8ae82755
Angel Tenev <55846518+angelntenev@users.noreply.github.com>
2022-01-31 21:20:44 +0000

    SFML square in circle created

---
+++ practical_1/main.cpp
@@ -0,0 +1,35 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    float screenSize = 500.f;
+    float radius = 100.f;
+  sf::RenderWindow window(sf::VideoMode(screenSize, screenSize), "SFML works!");
+  sf::CircleShape circleShape(radius);
+  sf::RectangleShape boxShape(sf::Vector2f(radius * sqrt(2), radius * sqrt(2)));
+  circleShape.setFillColor(sf::Color::Magenta);
+  boxShape.setFillColor(sf::Color::White);
+  circleShape.setOrigin(sf::Vector2f(radius, radius));
+  circleShape.setPosition(sf::Vector2f(screenSize/2, screenSize/2));
+  boxShape.setPosition(sf::Vector2f(screenSize/2, screenSize/2));
+  boxShape.setOrigin(sf::Vector2f((radius * sqrt(2)) / 2, (radius * sqrt(2)) / 2));
+
+  while (window.isOpen()){
+      sf::Event event;
+      while (window.pollEvent(event)){
+      if (event.type == sf::Event::Closed){
+        window.close();
+      }
+    }
+    window.clear();
+    
+    window.draw(circleShape);
+    window.draw(boxShape);
+    window.display();
+  }
+  return 0;
+}
